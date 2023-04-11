2895638b1a2e5a708e46bfb4dd570b8f5d70439f
Angelo Leva <angelokta7@gmail.com>
2023-04-10 19:05:02 -0500

    feat(FavoriteHit): add FavoriteHit component

---
+++ src/components/FavoriteHit/FavoriteHit.tsx
@@ -0,0 +1,47 @@
+import { IHit } from "../../types";
+import React, { useState, useEffect, useRef } from "react";
+import { timeSince } from "../../utils";
+import styles from "../Hit/styles/Hit.module.scss";
+import clock from "../../assets/clock.svg";
+import favorite from "../../assets/favorite.svg";
+import favoriteActive from "../../assets/favorite-active.svg";
+
+const FavoriteHit: React.FC<IHit> = ({
+  story_title,
+  created_at,
+  author,
+  story_url,
+  isFavorite,
+  toggleFavorite,
+}) => {
+  const [isCurrrentlyFavorite, setIsCurrentlyFavorite] = useState(isFavorite);
+
+  const handleFavorite = () => {
+    setIsCurrentlyFavorite(!isCurrrentlyFavorite);
+    toggleFavorite();
+  };
+
+  const imageSrc = isCurrrentlyFavorite ? favoriteActive : favorite;
+
+  return (
+    <div className={styles.hit}>
+      <div className={styles.hit__content}>
+        <div className={styles.hit__heading}>
+          <img src={clock} />
+          <span>
+            {timeSince(created_at)} by {author}
+          </span>
+        </div>
+
+        <a target="_blank" href={`${story_url}`}>
+          <h3>{story_title}</h3>
+        </a>
+      </div>
+      <div className={styles.hit__actions}>
+        <img src={imageSrc} onClick={handleFavorite} />
+      </div>
+    </div>
+  );
+};
+
+export default FavoriteHit;
---
+++ src/components/FavoriteHit/index.ts
@@ -0,0 +1,1 @@
+export { default as FavoriteHit } from './FavoriteHit';
