5c04588a62bfd719019bc43b8482d066d6bd5b1d
Angelo Leva <angelokta7@gmail.com>
2023-04-10 19:07:14 -0500

    feat(Constants): add constant files

---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+export const BASE_API_URL = "https://hn.algolia.com/api/v1";
+
+export const APP_ROUTES = [
+  { title: "All", route: `/` },
+  { title: "My faves", route: `/favorites` },
+];
+
+export const HITS_FILTERS = [
+  { value: "angular", label: "Angular" },
+  { value: "react", label: "ReactJs" },
+  { value: "vue", label: "Vuejs" },
+];
