dc913a7730baac99ab0310ff973bec0c00ad5535
Angelo Leva <angelokta7@gmail.com>
2023-04-10 19:09:00 -0500

    feat(Hooks):add useFetch hook to handle data

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { IHit } from "../types";
+import { transformData } from "../utils";
+
+export const useFetch = (query: string, page: number) => {
+  const [hits, setHits] = useState<IHit[]>([]);
+
+  const fetchHits = async () => {
+    try {
+      const response = await fetch(
+        `/api/search_by_date?query=${query}&page=${page}`
+      );
+      const { hits } = await response.json();
+
+      return transformData(hits);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(`Failed to fetch hits: ${error.message}`);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const getHits = async () => {
+      const data = await fetchHits();
+      setHits((prev) => [...prev, ...data!]);
+    };
+    getHits();
+  }, [page]);
+
+  useEffect(() => {
+    const updateHits = async () => {
+      const hits = await fetchHits();
+      setHits([...hits!]);
+    };
+    updateHits();
+  }, [query]);
+
+  return {
+    hits,
+  };
+};
